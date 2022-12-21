1b8c8dd52f1e7a18a1b94cf12687c401f6100247
Rahul kapoor <kapoorrahul304@gmail.com>
2022-12-21 12:58:02 +0530

    some improvment in this assign

--- block-BJaafo/code/script.js
+++ block-BJaafo/code/script.js
@@ -1,3 +1,28 @@
   function fullName(firstName , lastName){
-return firstName + lastName;
+return firstName + ' ' + lastName;
 }
+fullName('rahul' ,'kapoor');
+
+
+ let result = fullName('rahul','kapoor');
+
+ let expected = 'rahul sharma'
+ if(result !== expected){
+    throw new Error(`${result}not equal to ${expected}`)
+ };
+  result = fullName('rahul','kapoor');
+
+  expected = 'rahul kapoor'
+ if(result !== expected){
+    throw new Error(`${result}not equal to ${expected}`)
+ };
+
+
+ function totalAmount(amount,taxRate){
+    return `amount + (amount * taxRate) `
+ };
+  result = totalAmount(100,3);
+  expected = 300;
+  if(result !== expected){
+    throw new Error(` ${result} not equal to ${expected}`)
+  };
